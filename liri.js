ed3bda1992209ebb6ff9722c51aeb1562d97ebe5
Daniel Barkley <danielbarkley@daniels-mbp-2.attlocal.net>
2019-03-01 07:24:16 -0600

    Added all of the npm modules and require Var's.

--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
+
+
 require("dotenv").config();
 
 var spotify = new Spotify(keys.spotify);
+
+
+var Spotify = require('node-spotify-api');
+
+var moment = require('moment');
+
+var axios = require("axios");
