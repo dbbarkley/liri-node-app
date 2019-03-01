710763be792e5570d52605489b8db37c4b77b890
Daniel Barkley <danielbarkley@daniels-mbp-2.attlocal.net>
2019-02-28 23:44:03 -0600

    Added API Keys.

--- keys.js
+++ keys.js
@@ -4,3 +4,7 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+//OMDB KEY 2a43d746
+
+// BandInTown KEY "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
